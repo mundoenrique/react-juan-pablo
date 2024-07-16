d04479f81176b81ed27fdb7a51e42ec15b3b5a1d
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-16 15:19:50 -0400

    ternario

--- 00-IntroduccionJS/js/21.js
+++ 00-IntroduccionJS/js/21.js
@@ -5,8 +5,6 @@ const saldo = 1000;
 const pagar = 1200;
 const tarjeta = true;
 
-// auth ?
-//     console.log('Usuario autenticado') :
-//     console.log('No Autenticado, ir a Login')
+auth ? console.log('Usuario autenticado') : console.log('No Autenticado, ir a Login');
 
 saldo > pagar || tarjeta ? console.log('Si puedes pagar') : console.log('no, no puedes pagar');
