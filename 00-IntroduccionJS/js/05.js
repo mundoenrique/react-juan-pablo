bd864c5910c24b64485f60c28b3672f8623bbeaa
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-16 12:37:03 -0400

    Manipulación de objetos

--- 00-IntroduccionJS/js/05.js
+++ 00-IntroduccionJS/js/05.js
@@ -1,20 +1,44 @@
 // Objetos - Manipulación
-const producto = {
+const producto1 = {
   nombre: 'Tablet',
   precio: 300,
   disponible: false,
 };
 
-// Object.freeze(producto);
-// Object.seal(producto);
+const producto2 = {
+  nombre: 'Tablet',
+  precio: 300,
+  disponible: false,
+};
+
+const producto3 = {
+  nombre: 'Tablet',
+  precio: 300,
+  disponible: false,
+};
+
+console.log({ producto1 });
+console.log({ producto2 });
+console.log({ producto3 });
+
+Object.freeze(producto2);
+Object.seal(producto2);
 
 // Reescribir un valor
-producto.disponible = true;
+producto1.disponible = true;
+producto2.disponible = true;
+producto3.disponible = true;
 
 // Sino existe, lo va a añadir
-producto.imagen = 'imagen.jpg';
+producto1.imagen = 'imagen.jpg';
+producto2.imagen = 'imagen.jpg';
+producto3.imagen = 'imagen.jpg';
 
 // Eliminar propiedad
-delete producto.precio;
+delete producto1.precio;
+delete producto2.precio;
+delete producto3.precio;
 
-console.log(producto);
+console.log({ producto1 });
+console.log({ producto2 });
+console.log({ producto3 });
