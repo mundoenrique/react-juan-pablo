9ba6bb1019f523c3c45034be2c31549fb782f400
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-16 14:06:12 -0400

    funciones

--- 00-IntroduccionJS/js/14.js
+++ 00-IntroduccionJS/js/14.js
@@ -1,12 +1,16 @@
-sumar(10, 20);
-sumar(300, 1);
-sumar(100);
-
-// // Funciones - Function Expression
-const sumar = function (numero1 = 0, numero2 = 0) {
+// Funciones - Function Expression
+const sumar1 = function (numero1 = 0, numero2 = 0) {
   console.log(numero1 + numero2);
 };
 
-function sumar(numero1 = 0, numero2 = 0) {
+sumar1(10, 20);
+sumar1(300, 1);
+sumar1(100);
+
+sumar2(10, 20);
+sumar2(300, 1);
+sumar2(100);
+
+function sumar2(numero1 = 0, numero2 = 0) {
   console.log(numero1 + numero2);
 }
