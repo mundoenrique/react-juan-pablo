acfbc75d1fe4442e53fa52e137433355a8541065
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-16 12:57:55 -0400

    manejo de arreglos

--- 00-IntroduccionJS/js/09.js
+++ 00-IntroduccionJS/js/09.js
@@ -2,4 +2,7 @@
 const tecnologias = [20, 30, 40, true, 'React.js'];
 const index = 4;
 
+console.table(tecnologias);
+console.log({ index });
 console.log(tecnologias[index]);
+console.log(typeof tecnologias[index]);
