ff215eaed273ddd0a7d2a34d3dc994dd4ae4da0b
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-16 14:14:52 -0400

    diferecnia entre funciones declardas, referecniadas, de flecha

--- 00-IntroduccionJS/js/16.js
+++ 00-IntroduccionJS/js/16.js
@@ -1,15 +1,19 @@
 // Funciones que retornan valores
 
-const sumar = function (numero1 = 0, numero2 = 0) {
-  return numero1 + numero2;
-};
-
-function sumar(numero1 = 0, numero2 = 0) {
+function sumar1(numero1 = 0, numero2 = 0) {
   return numero1 + numero2;
 }
 
-const sumar = (numero1 = 0, numero2 = 0) => numero1 + numero2;
+const sumar2 = function (numero1 = 0, numero2 = 0) {
+  return numero1 + numero2;
+};
 
-const resultado = sumar(10, 20);
+const sumar3 = (numero1 = 0, numero2 = 0) => numero1 + numero2;
 
-console.log(resultado);
+const resultado1 = sumar1(10, 20);
+const resultado2 = sumar2(10, 20);
+const resultado3 = sumar3(10, 20);
+
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
