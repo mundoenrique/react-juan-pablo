f795cad7a23d23b7fc278370095d6a78e053e8d6
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-16 13:45:27 -0400

    Recorrer arreglos

--- 00-IntroduccionJS/js/12.js
+++ 00-IntroduccionJS/js/12.js
@@ -1,22 +1,31 @@
 const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React.js', 'Node.js', 'Nest.js', 'TypeScript'];
+console.table(tecnologias);
 
+// for
 for (let i = 0; i < tecnologias.length; i++) {
-  console.log(tecnologias[i]);
+  console.log(i, tecnologias[i]);
 }
 
-// forEach;
-tecnologias.forEach(function (tech) {
-  console.log(tech);
+// forEach
+tecnologias.forEach(function (tech, index) {
+  console.log(index, tech);
+  return tech;
 });
 
 // map
 const arrayMap = tecnologias.map(function (tech) {
+  console.log(tech);
   return tech;
 });
+
+console.table(arrayMap);
 
 // for ... of
 for (let tech of tecnologias) {
   console.log(tech);
 }
 
-console.log(arrayMap);
+// for ... in
+for (let tech in tecnologias) {
+  console.log(tech);
+}
