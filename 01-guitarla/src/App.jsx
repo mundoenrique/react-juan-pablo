ac3337c9b1a14b4bb6e70560fe1387404bf77981
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-17 10:51:54 -0400

    Elimina item del carrito

--- 01-guitarla/src/App.jsx
+++ 01-guitarla/src/App.jsx
@@ -1,10 +1,10 @@
-/* eslint-disable no-unused-vars */
 import Header from './components/Header';
 import Guitar from './components/Guitar';
 import Footer from './components/Footer';
 import { db } from './data/db';
 import { useState } from 'react';
 
+/* eslint-disable no-unused-vars */
 export default function App() {
   const [data, setData] = useState(db);
   const [cart, setCart] = useState([]);
@@ -21,9 +21,13 @@ export default function App() {
     }
   };
 
+  const removeFromCart = (id) => {
+    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
+  };
+
   return (
     <>
-      <Header cart={cart} />
+      <Header cart={cart} removeFromCart={removeFromCart} />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
--- 01-guitarla/src/components/Header.jsx
+++ 01-guitarla/src/components/Header.jsx
@@ -1,9 +1,8 @@
-/* eslint-disable react/prop-types */
-
 import { useMemo } from 'react';
 
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-export default function Header({ cart }) {
+export default function Header({ cart, removeFromCart }) {
   // state derivado
   const isEmpty = useMemo(() => cart.length === 0, [cart]);
   const cartTotal = useMemo(() => cart.reduce((total, item) => total + item.quantity * item.price, 0), [cart]);
@@ -53,7 +52,7 @@ export default function Header({ cart }) {
                               </button>
                             </td>
                             <td>
-                              <button className="btn btn-danger" type="button">
+                              <button className="btn btn-danger" type="button" onClick={() => removeFromCart(id)}>
                                 X
                               </button>
                             </td>
