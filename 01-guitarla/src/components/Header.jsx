900086fe20d51499f5709101ba494d59b411ab0f
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-17 10:27:38 -0400

    Calcúla total a pagar

--- 01-guitarla/src/components/Header.jsx
+++ 01-guitarla/src/components/Header.jsx
@@ -3,6 +3,7 @@
 export default function Header({ cart }) {
   // state derivado
   const isEmpty = () => cart.length === 0;
+  const cartTotal = () => cart.reduce((total, item) => total + item.quantity * item.price, 0);
   return (
     <header className="py-5 header">
       <div className="container-xl">
@@ -20,45 +21,47 @@ export default function Header({ cart }) {
                 {isEmpty() ? (
                   <p className="text-center">El carrito esta vacio</p>
                 ) : (
-                  <table className="w-100 table">
-                    <thead>
-                      <tr>
-                        <th>Imagen</th>
-                        <th>Nombre</th>
-                        <th>Precio</th>
-                        <th>Cantidad</th>
-                        <th></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {cart.map(({ id, name, price, quantity, image }) => (
-                        <tr key={id}>
-                          <td>
-                            <img className="img-fluid" src={`/img/${image}.jpg`} alt={`imagen ${name}`} />
-                          </td>
-                          <td>{name}</td>
-                          <td className="fw-bold">{price}</td>
-                          <td className="flex align-items-start gap-4">
-                            <button type="button" className="btn btn-dark">
-                              -
-                            </button>
-                            {quantity}
-                            <button type="button" className="btn btn-dark">
-                              +
-                            </button>
-                          </td>
-                          <td>
-                            <button className="btn btn-danger" type="button">
-                              X
-                            </button>
-                          </td>
+                  <>
+                    <table className="w-100 table">
+                      <thead>
+                        <tr>
+                          <th>Imagen</th>
+                          <th>Nombre</th>
+                          <th>Precio</th>
+                          <th>Cantidad</th>
+                          <th></th>
                         </tr>
-                      ))}
-                    </tbody>
-                  </table>
+                      </thead>
+                      <tbody>
+                        {cart.map(({ id, name, price, quantity, image }) => (
+                          <tr key={id}>
+                            <td>
+                              <img className="img-fluid" src={`/img/${image}.jpg`} alt={`imagen ${name}`} />
+                            </td>
+                            <td>{name}</td>
+                            <td className="fw-bold">${price}</td>
+                            <td className="flex align-items-start gap-4">
+                              <button type="button" className="btn btn-dark">
+                                -
+                              </button>
+                              {quantity}
+                              <button type="button" className="btn btn-dark">
+                                +
+                              </button>
+                            </td>
+                            <td>
+                              <button className="btn btn-danger" type="button">
+                                X
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </>
                 )}
                 <p className="text-end">
-                  Total pagar: <span className="fw-bold">$899</span>
+                  Total pagar: <span className="fw-bold">${cartTotal()}</span>
                 </p>
                 <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
               </div>
