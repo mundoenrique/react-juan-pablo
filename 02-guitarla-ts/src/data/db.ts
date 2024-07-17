fdd1a59fc46b3e626f82c4d18fd0fd1a61524fda
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-17 16:02:59 -0400

    GuitarLA en typescript

---
+++ 02-guitarla-ts/src/data/db.ts
@@ -0,0 +1,89 @@
+import type { Guitar } from '../types';
+
+export const db: Guitar[] = [
+  {
+    id: 1,
+    name: 'Lukather',
+    image: 'guitarra_01',
+    description: 'Aliqua reprehenderit ex aliquip velit est voluptate ad commodo occaecat ut sunt.',
+    price: 299,
+  },
+  {
+    id: 2,
+    name: 'SRV',
+    image: 'guitarra_02',
+    description: 'In proident nisi aute ipsum minim nulla ut quis elit voluptate fugiat commodo eu est.',
+    price: 349,
+  },
+  {
+    id: 3,
+    name: 'Borland',
+    image: 'guitarra_03',
+    description: 'Fugiat magna nisi minim quis Lorem in nostrud dolore consectetur laborum anim fugiat quis.',
+    price: 329,
+  },
+  {
+    id: 4,
+    name: 'VAI',
+    image: 'guitarra_04',
+    description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
+    price: 299,
+  },
+  {
+    id: 5,
+    name: 'Thompson',
+    image: 'guitarra_05',
+    description: 'Aute quis enim excepteur sit anim exercitation mollit amet.',
+    price: 399,
+  },
+  {
+    id: 6,
+    name: 'White',
+    image: 'guitarra_06',
+    description: 'Consequat cillum laborum enim ea ad ea mollit minim ea magna dolore.',
+    price: 329,
+  },
+  {
+    id: 7,
+    name: 'Cobain',
+    image: 'guitarra_07',
+    description: 'Anim dolor officia aute aute sit cupidatat esse mollit cupidatat do dolore sit mollit sit.',
+    price: 349,
+  },
+  {
+    id: 8,
+    name: 'Dale',
+    image: 'guitarra_08',
+    description:
+      'Veniam est ut cupidatat esse fugiat excepteur incididunt laborum ex adipisicing dolor aute ut occaecat.',
+    price: 379,
+  },
+  {
+    id: 9,
+    name: 'Krieger',
+    image: 'guitarra_09',
+    description: 'Eiusmod aute dolor do magna laborum.',
+    price: 289,
+  },
+  {
+    id: 10,
+    name: 'Campbell',
+    image: 'guitarra_10',
+    description: 'Est duis do veniam ex qui culpa aliquip est reprehenderit voluptate non.',
+    price: 349,
+  },
+  {
+    id: 11,
+    name: 'Reed',
+    image: 'guitarra_11',
+    description: 'Officia Lorem enim laborum laborum et incididunt enim.',
+    price: 399,
+  },
+  {
+    id: 12,
+    name: 'Hazel',
+    image: 'guitarra_12',
+    description: 'Sunt non deserunt occaecat anim non voluptate mollit.',
+    price: 379,
+  },
+];
