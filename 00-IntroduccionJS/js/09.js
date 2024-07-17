587bd390488eb6c4c72653128090cecbea6a273a
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-16 21:59:20 -0400

    Eventos de DOM

--- 00-IntroduccionJS/js/09.js
+++ 00-IntroduccionJS/js/09.js
@@ -1,4 +1,4 @@
-// Arrays o Arreglos
+// Arrays o Arreglos https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array
 const tecnologias = [20, 30, 40, true, 'React.js'];
 const index = 4;
 
--- 00-IntroduccionJS/js/30.js
+++ 00-IntroduccionJS/js/30.js
@@ -1,4 +1,4 @@
-// Eventos del DOM - Clicks
+// Eventos del DOM - Clicks https://developer.mozilla.org/es/docs/Web/API/Element
 const heading = document.querySelector('.heading');
 const enlaces = document.querySelectorAll('.navegacion a');
 
@@ -9,9 +9,9 @@ heading.addEventListener('mouseleave', () => {
   heading.textContent = 'Saliendo...';
 });
 
-enlaces.forEach((enlace) => {
+enlaces.forEach((enlace, index) => {
   enlace.addEventListener('click', (e) => {
     e.preventDefault();
-    e.target.textContent = 'Diste click...';
+    e.target.textContent = 'Diste click... ' + (index + 1);
   });
 });
