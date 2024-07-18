b53bd2b3a1ea9ab8b5ac7d26faa01c9725694131
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-18 15:18:07 -0400

    Inicia maqueta

--- 04-calorie-tracker/src/App.tsx
+++ 04-calorie-tracker/src/App.tsx
@@ -1,7 +1,18 @@
+import Form from './components/Form';
+
 export default function App() {
   return (
     <>
-      <h1 className="text-6xl">Contador de calorías</h1>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">Contador de calorias</h1>
+        </div>
+      </header>
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <Form />
+        </div>
+      </section>
     </>
   );
 }
