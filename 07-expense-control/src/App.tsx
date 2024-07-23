a4363b0099de82ff3b577974728521c9d5bd7b0e
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-23 11:32:10 -0400

    Componente para el seguimiento de gastos

--- 07-expense-control/src/App.tsx
+++ 07-expense-control/src/App.tsx
@@ -1,13 +1,22 @@
+import { useMemo } from 'react';
+import { useBudget } from './hooks/useBudget';
 import BudgetForm from './components/BudgetForm';
+import BudgetTraker from './components/BudgetTraker';
 
 export default function App() {
+  const {
+    state: { budget },
+  } = useBudget();
+
+  const isValidBudget = useMemo(() => budget > 0, [budget]);
+
   return (
     <>
       <header className="bg-blue-600 py-8 maxh-72">
         <h1 className="uppercase text-center font-black tex-4xl text-white">PLanificador de gastos</h1>
       </header>
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lgmt-10 p-10">
-        <BudgetForm />
+        {isValidBudget ? <BudgetTraker /> : <BudgetForm />}
       </div>
     </>
   );
