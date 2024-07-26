3a0205f07667aa0d2b10317439a75b05eca8ca64
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-26 11:55:44 -0400

    Almacena paciente en el estado

--- 09-patients-list/src/store.ts
+++ 09-patients-list/src/store.ts
@@ -1,10 +1,21 @@
+import uuid4 from 'uuid4';
 import { create } from 'zustand';
 
-import type { TpatientState } from './types';
+import type { TdrafPatient, Tpatient, TpatientState } from './types';
 
-export const usePatientStorage = create<TpatientState>(() => ({
+const createPatient = (drafPatient: TdrafPatient): Tpatient => {
+  const patient = { ...drafPatient, id: uuid4() };
+
+  return patient;
+};
+
+export const usePatientStorage = create<TpatientState>((set) => ({
   patients: [],
   addPatient: (data) => {
-    console.log(data);
+    const NewPatient = createPatient(data);
+
+    set((state) => ({
+      patients: [...state.patients, NewPatient],
+    }));
   },
 }));
