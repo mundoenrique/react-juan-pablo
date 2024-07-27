cf7ba1eeb9d6ca83e2a9c74f0596c8e2ef5abeb4
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-27 12:49:45 -0400

    Consulta el tiempo

--- 10-get-weather/src/hooks/useWeather.ts
+++ 10-get-weather/src/hooks/useWeather.ts
@@ -8,8 +8,15 @@ export default function useWeather() {
     try {
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
 
-      const { data } = await axios(geoUrl);
-      console.log(data);
+      const { data: dataGeo } = await axios(geoUrl);
+      const lat = dataGeo[0].lat;
+      const lon = dataGeo[0].lon;
+
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+
+      const { data: dataWeather } = await axios(weatherUrl);
+
+      console.log(dataWeather);
     } catch (error) {
       console.error(error);
     }
