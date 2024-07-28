b2484a06f5f020cd1700897d35224e5d8e9df3a4
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-27 23:27:08 -0400

    Crea estilos y maqueta inicial para la aplicación

--- 11-quote-crypto/src/App.tsx
+++ 11-quote-crypto/src/App.tsx
@@ -1,3 +1,12 @@
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="container">
+        <h1 className="app-title">
+          Cotizador de <span>Criptomonedas</span>
+        </h1>
+      </div>
+      <div className="content">API</div>
+    </>
+  );
 }
