f38c95d8c50fbb62fa8d41eb092b8093a0089cdc
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-28 01:17:43 -0400

    Axios para obtener 20 criptomonedas

--- 11-quote-crypto/src/storage.ts
+++ 11-quote-crypto/src/storage.ts
@@ -1,5 +1,16 @@
+import axios from 'axios';
 import { create } from 'zustand';
 
+async function getCrypto() {
+  const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD`;
+  const {
+    data: { Data },
+  } = await axios(url);
+  console.log(Data);
+}
+
 export const useCryptoStore = create(() => ({
-  fetchCryptos: () => {},
+  fetchCryptos: () => {
+    getCrypto();
+  },
 }));
