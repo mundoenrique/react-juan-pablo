69d9831ce25148fb6b792d7b24f547d82ebf035a
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-30 11:22:10 -0400

    Formulario para buscar recetas

--- 12-drinking-search/src/components/Header.tsx
+++ 12-drinking-search/src/components/Header.tsx
@@ -1,6 +1,10 @@
-import { Link, NavLink } from 'react-router-dom';
+import { useMemo } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 
 export default function Header() {
+  const { pathname } = useLocation();
+  const isHome = useMemo(() => pathname === '/', [pathname]);
+
   return (
     <header className="bg-slate-800">
       <div className="mx-auto container px-5 py-16">
@@ -22,6 +26,35 @@ export default function Header() {
             </NavLink>
           </nav>
         </div>
+        {isHome && (
+          <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+            <div className="space-y-4">
+              <label htmlFor="ingredient" className="block text-white uppercase font-extrabold text-lg">
+                Nombre o ingredientes
+              </label>
+              <input
+                id="ingredient"
+                name="ingredient"
+                type="text"
+                className="p-3 w-full rounded-lg focus:outline-none"
+                placeholder="Nombre o ingrediente"
+              />
+            </div>
+            <div className="space-y-4">
+              <label htmlFor="category" className="block text-white uppercase font-extrabold text-lg">
+                Categoría
+              </label>
+              <select id="category" name="category" className="p-3 w-full rounded-lg focus:outline-none">
+                <option value="">-- Seleccione</option>
+              </select>
+            </div>
+            <input
+              type="submit"
+              value="Buscar recetas"
+              className="cursor-pointer bg-orange-800 hover:orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"
+            />
+          </form>
+        )}
       </div>
     </header>
   );
