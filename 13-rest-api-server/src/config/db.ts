0edb730127786f41218775417c70d459228eab80
mundoenrique <enrique.penaloza.working@gmail.com>
2024-07-31 19:48:49 -0400

    Variables de entorno

--- 13-rest-api-server/src/config/db.ts
+++ 13-rest-api-server/src/config/db.ts
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
 import { Sequelize } from 'sequelize';
+
+dotenv.config();
 
 // const db = new Sequelize('postgres://admin_user:123456@localhost:5400/rest_api_db', {
 //   dialectOptions: { ssl: { require: false } },
@@ -6,6 +9,6 @@ import { Sequelize } from 'sequelize';
 
 // const db = new Sequelize('postgres://admin_user:123456@localhost:5400/rest_api_db?ssl=true');
 
-const db = new Sequelize('postgres://admin_user:123456@localhost:5400/rest_api_db');
+const db = new Sequelize(process.env.DATABASE_URL);
 
 export default db;
