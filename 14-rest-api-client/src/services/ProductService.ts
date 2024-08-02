5b20adb68561e8464d540624a154bde50cb57896
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-02 12:42:31 -0400

    Valida petición con valibot

--- 14-rest-api-client/src/services/ProductService.ts
+++ 14-rest-api-client/src/services/ProductService.ts
@@ -1,7 +1,18 @@
+import { safeParse } from 'valibot';
+import { DraftProductSchema } from '../types';
+
 type ProductData = {
   [k: string]: FormDataEntryValue;
 };
 
 export async function addProdcut(data: ProductData) {
-  console.log(data);
+  try {
+    const { success, output } = safeParse(DraftProductSchema, { name: data.name, price: +data.price });
+
+    if (success) {
+      console.log(output);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
