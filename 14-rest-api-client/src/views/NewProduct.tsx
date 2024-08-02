3b1d5dd2993f614c14b0b5958ea26d5b30e85722
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-02 08:01:31 -0400

    Formulario para crear producto

--- 14-rest-api-client/src/views/NewProduct.tsx
+++ 14-rest-api-client/src/views/NewProduct.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Form, Link } from 'react-router-dom';
 
 export default function NewProduct() {
   return (
@@ -12,6 +12,38 @@ export default function NewProduct() {
           Volver a productos
         </Link>
       </div>
+
+      <Form className="mt-10" method="post">
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="name">
+            Nombre Producto:
+          </label>
+          <input
+            id="name"
+            type="text"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Nombre del Producto"
+            name="name"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="price">
+            Precio:
+          </label>
+          <input
+            id="price"
+            type="number"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Precio Producto. ej. 200, 300"
+            name="price"
+          />
+        </div>
+        <input
+          type="submit"
+          className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+          value="Registrar Producto"
+        />
+      </Form>
     </>
   );
 }
