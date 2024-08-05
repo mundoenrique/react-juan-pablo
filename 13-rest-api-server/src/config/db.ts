973ed81b0884c42690e972d296a4b12c210d6e65
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-05 13:14:45 -0400

    Ajustes para el build del back

--- 13-rest-api-server/src/config/db.ts
+++ 13-rest-api-server/src/config/db.ts
@@ -10,7 +10,7 @@ dotenv.config();
 // const db = new Sequelize('postgres://admin_user:123456@localhost:5400/rest_api_db?ssl=true');
 
 const db = new Sequelize(process.env.DATABASE_URL, {
-  models: [__dirname + '/../models/**/*.ts'],
+  models: [__dirname + '/../models/**/*'],
   logging: false,
 });
 
