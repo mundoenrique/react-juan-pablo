c8a4215d02d9446dd346a18779d2fe35cb9599dd
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-05 20:51:48 -0400

    Crea registros en mongo

--- 15-uptasks-server/src/controllers/ProjectController.ts
+++ 15-uptasks-server/src/controllers/ProjectController.ts
@@ -1,7 +1,24 @@
 import type { Request, Response } from 'express';
+import Project from '../models/Project';
 
 export class ProjectController {
+  static createProject = async (req: Request, res: Response) => {
+    const project = new Project(req.body);
+    try {
+      await project.save();
+      const payload = req.body;
+      res.json({ data: `createProject by: ${req.method}`, payload });
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
   static getAllProjects = async (req: Request, res: Response) => {
-    res.send('Todos los proyectos');
+    res.json({ data: `getAllProjects by: ${req.method}` });
+  };
+
+  static Project = async (req: Request, res: Response) => {
+    const payload = req.body;
+    res.json({ data: `... Project by: ${req.method}`, payload });
   };
 }
