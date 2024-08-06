ec2868aeaa7f9da208a1d7e78104649ededaa27c
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-06 11:33:45 -0400

    valida el la creación de proyecto

--- 15-uptasks-server/src/routes/projectRoutes.ts
+++ 15-uptasks-server/src/routes/projectRoutes.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import { body, param } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectController';
+import { handleInputErrors } from '../middleware/validation';
 
 const router = Router();
 
-router.post('/', ProjectController.createProject);
+router.post(
+  '/',
+  body('projectName').notEmpty().withMessage('EL nombre del proyecto es obligatorio'),
+  body('clientName').notEmpty().withMessage('EL nombre del cliente es obligatorio'),
+  body('description').notEmpty().withMessage('La descripción del proyecto es obligatoria'),
+  handleInputErrors,
+  ProjectController.createProject
+);
 router.get('/', ProjectController.getAllProjects);
 
 export default router;
