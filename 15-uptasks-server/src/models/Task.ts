a2ef1946e968e050f9a7d4a6b163add486f5f34b
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-06 13:37:42 -0400

    Define el estado para las tareas

--- 15-uptasks-server/src/models/Task.ts
+++ 15-uptasks-server/src/models/Task.ts
@@ -1,9 +1,19 @@
 import mongoose, { Schema, Document, Types } from 'mongoose';
 
+const taskStatus = {
+  PENDING: 'pending',
+  ON_HOLD: 'onHold',
+  IN_PROGRESS: 'inProgress',
+  UNDER_REVIEW: 'underReview',
+  COMPLETED: 'completed',
+} as const;
+
+export type TaskStatus = (typeof taskStatus)[keyof typeof taskStatus];
 export interface ITask extends Document {
   name: string;
   description: string;
   project: Types.ObjectId;
+  status: TaskStatus;
 }
 
 const taskSchema: Schema = new Schema(
@@ -21,6 +31,11 @@ const taskSchema: Schema = new Schema(
     porject: {
       type: Types.ObjectId,
       ref: 'Project',
+    },
+    status: {
+      type: String,
+      enum: Object.values(taskStatus),
+      default: taskStatus.PENDING,
     },
   },
   { timestamps: true }
