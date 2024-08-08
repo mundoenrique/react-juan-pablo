0a3c3b49cd16b050dea8d74186937d05173c65fb
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-07 22:03:07 -0400

    Primeras rutas

--- 16-uptasks-client/src/router.tsx
+++ 16-uptasks-client/src/router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppLayout from './layouts/AppLayout';
+import DashboardView from './views/DashboardView';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route>
-          <Route path="/" index />
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<DashboardView />} index />
         </Route>
       </Routes>
     </BrowserRouter>
