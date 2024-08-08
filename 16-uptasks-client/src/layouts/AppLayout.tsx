3d44b74d1560254c5bc5f2b55f68f7aedd1b85f7
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-07 22:50:48 -0400

    Layout principal

--- 16-uptasks-client/src/layouts/AppLayout.tsx
+++ 16-uptasks-client/src/layouts/AppLayout.tsx
@@ -1,10 +1,28 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+import Logo from '@/components/Logo';
+import NavMenu from '@/components/NavMenu';
 
 export default function AppLayout() {
   return (
     <>
-      <div>AppLayout</div>
-      <Outlet />;
+      <header className="bg-gray-800 py-5">
+        <div className=" max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+          <div className="w-64">
+            <Link to={'/'}>
+              <Logo />
+            </Link>
+          </div>
+          <NavMenu />
+        </div>
+      </header>
+
+      <section className="max-w-screen-2xl mx-auto mt-10 p-5">
+        <Outlet />
+      </section>
+
+      <footer className="py-5">
+        <p className="text-center">Todos los derechos reservados {new Date().getFullYear()}</p>
+      </footer>
     </>
   );
 }
