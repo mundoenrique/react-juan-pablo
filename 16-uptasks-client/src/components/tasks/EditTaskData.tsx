e93b89d8237bdb94240c68a1796ad7850d48ee5e
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-15 19:07:36 -0400

    envia petición para actulizar la tarea

--- 16-uptasks-client/src/components/tasks/EditTaskData.tsx
+++ 16-uptasks-client/src/components/tasks/EditTaskData.tsx
@@ -1,4 +1,4 @@
-import { useLocation, useParams } from 'react-router-dom';
+import { Navigate, useLocation, useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { getTaskById } from '@/api/TaskAPI';
 import EditTaskModal from './EditTaskModal';
@@ -9,11 +9,13 @@ export default function EditTaskData() {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const taskId = queryParams.get('editTask')!;
-  const { data } = useQuery({
+  const { data, isError } = useQuery({
     queryKey: ['task', taskId],
     queryFn: () => getTaskById({ taskId, projectId }),
     enabled: !!taskId,
   });
 
-  if (data) return <EditTaskModal data={data} />;
+  if (isError) return <Navigate to={'/404'} />;
+
+  if (data) return <EditTaskModal data={data} taskId={taskId} />;
 }
