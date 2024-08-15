685c4211f8cbdefb74f10fc971dd7e1c58577d77
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-15 16:22:35 -0400

    Estilos para el listado de estados de las tareas

--- 16-uptasks-client/src/components/tasks/TaskList.tsx
+++ 16-uptasks-client/src/components/tasks/TaskList.tsx
@@ -3,13 +3,14 @@ import TaskCard from './TaskCard';
 
 type TaskListProps = {
   tasks: Task[];
+  canEdit: boolean;
 };
 
-type groupedTasks = {
+type GroupedTasks = {
   [key: string]: Task[];
 };
 
-const inititialStatusGroups: groupedTasks = {
+const initialStatusGroups: GroupedTasks = {
   pending: [],
   onHold: [],
   inProgress: [],
@@ -17,12 +18,28 @@ const inititialStatusGroups: groupedTasks = {
   completed: [],
 };
 
+const statusStyles: { [key: string]: string } = {
+  pending: 'border-t-slate-500',
+  onHold: 'border-t-red-500',
+  inProgress: 'border-t-blue-500',
+  underReview: 'border-t-amber-500',
+  completed: 'border-t-emerald-500',
+};
+
+const statusTranslations: { [key: string]: string } = {
+  pending: 'Pendiente',
+  onHold: 'En Espera',
+  inProgress: 'En Progreso',
+  underReview: 'En Revisión',
+  completed: 'Completado',
+};
+
 export default function TaskList({ tasks }: TaskListProps) {
   const groupedTasks = tasks.reduce((acc, task) => {
     let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
     currentGroup = [...currentGroup, task];
     return { ...acc, [task.status]: currentGroup };
-  }, inititialStatusGroups);
+  }, initialStatusGroups);
 
   console.log(groupedTasks);
 
@@ -33,6 +50,11 @@ export default function TaskList({ tasks }: TaskListProps) {
       <div className="flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32">
         {Object.entries(groupedTasks).map(([status, tasks]) => (
           <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
+            <h3
+              className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]} `}
+            >
+              {statusTranslations[status]}
+            </h3>
             <ul className="mt-5 space-y-5">
               {tasks.length === 0 ? (
                 <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
