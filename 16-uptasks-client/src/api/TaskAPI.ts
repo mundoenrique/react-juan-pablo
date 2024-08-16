b2984f3c13ed1fecb84be265140f244ffd3b79e1
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-16 13:06:02 -0400

    Petición de actulización  del estado de una tarea

--- 16-uptasks-client/src/api/TaskAPI.ts
+++ 16-uptasks-client/src/api/TaskAPI.ts
@@ -63,3 +63,16 @@ export async function deleteTask({ projectId, taskId }: Pick<TaskAPI, 'projectId
     }
   }
 }
+
+export async function updateStatus({ projectId, taskId, status }: Pick<TaskAPI, 'projectId' | 'taskId' | 'status'>) {
+  try {
+    const url = `/projects/${projectId}/tasks/${taskId}/status`;
+    const { data } = await api.post<string>(url, { status });
+
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
--- 16-uptasks-client/src/components/tasks/TaskModalDetails.tsx
+++ 16-uptasks-client/src/components/tasks/TaskModalDetails.tsx
@@ -1,11 +1,12 @@
-import { Fragment } from 'react';
+import { ChangeEvent, Fragment } from 'react';
 import { Navigate, useLocation, useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
-import { useQuery } from '@tanstack/react-query';
-import { getTaskById } from '@/api/TaskAPI';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { getTaskById, updateStatus } from '@/api/TaskAPI';
 import { formatDate } from '@/utils/utils';
 import { statusTranslations } from '@/locales/es';
+import { TaskStatus } from '@/types/index';
 
 export default function TaskModalDetails() {
   const params = useParams();
@@ -15,12 +16,32 @@ export default function TaskModalDetails() {
   const queryParams = new URLSearchParams(location.search);
   const taskId = queryParams.get('viewTask')!;
   const show = taskId ? true : false;
+
   const { data, isError, error } = useQuery({
     queryKey: ['task', taskId],
     queryFn: () => getTaskById({ projectId, taskId }),
     enabled: !!taskId,
     retry: false,
   });
+
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: updateStatus,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      toast.success(data);
+      queryClient.invalidateQueries({ queryKey: ['project', projectId] });
+      queryClient.invalidateQueries({ queryKey: ['task', taskId] });
+    },
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const status = e.target.value as TaskStatus;
+    const data = { projectId, taskId, status };
+    mutate(data);
+  };
 
   if (isError) {
     toast.error(error.message, { toastId: 'error' });
@@ -65,7 +86,11 @@ export default function TaskModalDetails() {
                     <p className="text-lg text-slate-500 mb-2">Descripción: {data.description} </p>
                     <div className="my-5 space-y-3">
                       <label className="font-bold">Estado Actual:</label>
-                      <select className="w-full p-3 bg-white border border-gray-300" defaultValue={data.status}>
+                      <select
+                        className="w-full p-3 bg-white border border-gray-300"
+                        defaultValue={data.status}
+                        onChange={handleChange}
+                      >
                         {Object.entries(statusTranslations).map(([key, value]) => (
                           <option key={key} value={key}>
                             {value}
