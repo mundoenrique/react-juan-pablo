d2aa52da5c08627c2242fa66142c7099a5594fa9
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-17 13:59:59 -0400

    REgistro de usuario

--- 16-uptasks-client/src/router.tsx
+++ 16-uptasks-client/src/router.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AppLayout from '@/layouts/AppLayout';
+import AuthLayout from './layouts/AuthLayout';
 import DashboardView from '@/views/DashboardView';
+import LoginView from './views/auth/LoginView';
+import RegisterView from './views/auth/RegisterView';
 import CreateProjectView from './views/projects/CreateProjectView';
 import EditProjectView from './views/projects/EditProjectView';
 import ProjectDetailsView from './views/projects/ProjectDetailsView';
@@ -14,6 +17,10 @@ export default function Router() {
           <Route path="/projects/create" element={<CreateProjectView />} />
           <Route path="/projects/:projectId" element={<ProjectDetailsView />} />
           <Route path="/projects/:projectId/edit" element={<EditProjectView />} />
+        </Route>
+        <Route element={<AuthLayout />}>
+          <Route path="/auth/login" element={<LoginView />} />
+          <Route path="/auth/register" element={<RegisterView />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ 16-uptasks-client/src/views/auth/ConfirmAccountView.tsx
@@ -0,0 +1,3 @@
+export default function ConfirmAccountView() {
+  return <div>ConfirmAccountView</div>;
+}
