f26a23d087bdae33e47f4f2bb9d3959896a3a9e8
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-19 13:52:04 -0400

    Ajusta que el middleware authenticate se ejecute en todos los endpoints de proyecto

--- 15-uptasks-server/src/routes/projectRoutes.ts
+++ 15-uptasks-server/src/routes/projectRoutes.ts
@@ -8,10 +8,11 @@ import { taskBelongsToProject, taskExists } from '../middleware/task';
 import { authenticate } from '../middleware/auth';
 
 const router = Router();
+router.use(authenticate);
 
 router.post(
   '/',
-  authenticate,
+
   body('projectName').notEmpty().withMessage('El nombre del proyecto es obligatorio'),
   body('clientName').notEmpty().withMessage('El nombre del cliente es obligatorio'),
   body('description').notEmpty().withMessage('La descripción del proyecto es obligatoria'),
@@ -19,7 +20,7 @@ router.post(
   ProjectController.createProject
 );
 
-router.get('/', authenticate, ProjectController.getAllProjects);
+router.get('/', ProjectController.getAllProjects);
 
 router.get(
   '/:id',
