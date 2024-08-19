3106b0b686841925f29e7538881ec77a15eaa0bd
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-19 10:32:02 -0400

    Valida e token y cambia la contraseña

--- 15-uptasks-server/src/controllers/AuthController.ts
+++ 15-uptasks-server/src/controllers/AuthController.ts
@@ -172,4 +172,42 @@ export class AuthController {
       res.status(500).json({ error: 'Hubo un error' });
     }
   };
+
+  static validateToken = async (req: Request, res: Response) => {
+    try {
+      const { token } = req.body;
+      const tokenExists = await Token.findOne({ token });
+
+      if (!tokenExists) {
+        const error = new Error('Token no válido');
+        return res.status(404).json({ error: error.message });
+      }
+
+      res.send('Token válido, Define tu nuevo password');
+    } catch (error) {
+      res.status(500).json({ error: 'Hubo un error' });
+    }
+  };
+
+  static updatePasswordWithToken = async (req: Request, res: Response) => {
+    try {
+      const { token } = req.params;
+      const { password } = req.body;
+      const tokenExists = await Token.findOne({ token });
+
+      if (!tokenExists) {
+        const error = new Error('Token no válido');
+        return res.status(404).json({ error: error.message });
+      }
+
+      const user = await User.findById(tokenExists.user);
+      user.password = await hashPassword(password);
+
+      await Promise.allSettled([user.save(), tokenExists.deleteOne()]);
+
+      res.send('El password se modificó correctamente');
+    } catch (error) {
+      res.status(500).json({ error: 'Hubo un error' });
+    }
+  };
 }
--- 15-uptasks-server/src/routes/authRoutes.ts
+++ 15-uptasks-server/src/routes/authRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { body } from 'express-validator';
+import { body, param } from 'express-validator';
 import { AuthController } from '../controllers/AuthController';
 import { handleInputErrors } from '../middleware/validation';
 
@@ -48,6 +48,28 @@ router.post(
   body('email').isEmail().withMessage('E-mail no válido'),
   handleInputErrors,
   AuthController.forgotPassword
+);
+
+router.post(
+  '/validate-token',
+  body('token').notEmpty().withMessage('El Token no puede ir vacio'),
+  handleInputErrors,
+  AuthController.validateToken
+);
+
+router.post(
+  '/update-password/:token',
+  param('token').isNumeric().withMessage('Token no válido'),
+  body('password').isLength({ min: 8 }).withMessage('El password es muy corto, minimo 8 caracteres'),
+  body('password_confirmation').custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Los Password no son iguales');
+    }
+
+    return true;
+  }),
+  handleInputErrors,
+  AuthController.updatePasswordWithToken
 );
 
 export default router;
