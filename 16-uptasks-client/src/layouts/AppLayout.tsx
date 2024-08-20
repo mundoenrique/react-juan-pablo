c580670f8eb925325ba23333b9ae5a300012d8f5
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-19 21:02:23 -0400

    Valida usuario atutenticado

--- 16-uptasks-client/src/layouts/AppLayout.tsx
+++ 16-uptasks-client/src/layouts/AppLayout.tsx
@@ -1,31 +1,41 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Navigate, Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Logo from '@/components/Logo';
 import NavMenu from '@/components/NavMenu';
+import { useAuth } from '@/hooks/useAuth';
 
 export default function AppLayout() {
-  return (
-    <>
-      <header className="bg-gray-800 py-5">
-        <div className=" max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
-          <div className="w-64">
-            <Link to={'/'}>
-              <Logo />
-            </Link>
+  const { data, isError, isLoading } = useAuth();
+
+  if (isLoading) return 'Cargando...';
+
+  if (isError) {
+    return <Navigate to={'/auth/login'} />;
+  }
+
+  if (data)
+    return (
+      <>
+        <header className="bg-gray-800 py-5">
+          <div className=" max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+            <div className="w-64">
+              <Link to={'/'}>
+                <Logo />
+              </Link>
+            </div>
+            <NavMenu />
           </div>
-          <NavMenu />
-        </div>
-      </header>
+        </header>
 
-      <section className="max-w-screen-2xl mx-auto mt-10 p-5">
-        <Outlet />
-      </section>
+        <section className="max-w-screen-2xl mx-auto mt-10 p-5">
+          <Outlet />
+        </section>
 
-      <footer className="py-5">
-        <p className="text-center">Todos los derechos reservados {new Date().getFullYear()}</p>
-      </footer>
-      <ToastContainer pauseOnHover={false} pauseOnFocusLoss={false} />
-    </>
-  );
+        <footer className="py-5">
+          <p className="text-center">Todos los derechos reservados {new Date().getFullYear()}</p>
+        </footer>
+        <ToastContainer pauseOnHover={false} pauseOnFocusLoss={false} />
+      </>
+    );
 }
