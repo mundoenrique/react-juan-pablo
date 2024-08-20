3ece9c927ca56283b4eb779e71a0df469b38c605
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-19 21:13:42 -0400

    Cierra la sesión

--- 16-uptasks-client/src/components/NavMenu.tsx
+++ 16-uptasks-client/src/components/NavMenu.tsx
@@ -3,12 +3,20 @@ import { Popover, PopoverButton, PopoverPanel, Transition } from '@headlessui/re
 import { Bars3Icon } from '@heroicons/react/20/solid';
 import { Link } from 'react-router-dom';
 import { User } from '../types';
+import { useQueryClient } from '@tanstack/react-query';
 
 type NavMenuProps = {
   name: User['name'];
 };
 
 export default function NavMenu({ name }: NavMenuProps) {
+  const queryClient = useQueryClient();
+
+  const logout = () => {
+    localStorage.removeItem('AUTH_TOKEN');
+    queryClient.invalidateQueries({ queryKey: ['user'] });
+  };
+
   return (
     <Popover className="relative">
       <PopoverButton className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 p-1 rounded-lg bg-purple-400">
@@ -33,7 +41,7 @@ export default function NavMenu({ name }: NavMenuProps) {
             <Link to="/" className="block p-2 hover:text-purple-950">
               Mis Proyectos
             </Link>
-            <button className="block p-2 hover:text-purple-950" type="button" onClick={() => {}}>
+            <button className="block p-2 hover:text-purple-950" type="button" onClick={logout}>
               Cerrar Sesión
             </button>
           </div>
