c3230ab47783b6264bb4d1ddcbe574eccd5da83a
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-20 18:52:27 -0400

    Restricción de acciones en el proyecto

--- 16-uptasks-client/src/views/DashboardView.tsx
+++ 16-uptasks-client/src/views/DashboardView.tsx
@@ -6,6 +6,7 @@ import { EllipsisVerticalIcon } from '@heroicons/react/20/solid';
 import { deleteProject, getProjects } from '@/api/ProjectAPI';
 import { toast } from 'react-toastify';
 import { useAuth } from '@/hooks/useAuth';
+import { isManager } from '@/utils/policies';
 
 export default function DashboardView() {
   const { data: user, isLoading: authLading } = useAuth();
@@ -48,6 +49,17 @@ export default function DashboardView() {
               <li key={project._id} className="flex justify-between gap-x-6 px-5 py-10">
                 <div className="flex min-w-0 gap-x-4">
                   <div className="min-w-0 flex-auto space-y-2">
+                    <div className="mb-2">
+                      {isManager(project.manager, user._id) ? (
+                        <p className="font-bold text-xs uppercase bg-indigo-50 text-indigo-500 border-2 border-indigo-500 rounded-lg inline-block py-1 px-5">
+                          Manager
+                        </p>
+                      ) : (
+                        <p className="font-bold text-xs uppercase bg-green-50 text-green-500 border-2 border-green-500 rounded-lg inline-block py-1 px-5">
+                          Colaborador
+                        </p>
+                      )}
+                    </div>
                     <Link
                       to={`/projects/${project._id}`}
                       className="text-gray-600 cursor-pointer hover:underline text-3xl font-bold"
@@ -82,7 +94,7 @@ export default function DashboardView() {
                             Ver Proyecto
                           </Link>
                         </MenuItem>
-                        {project.manager === user._id && (
+                        {isManager(project.manager, user._id) && (
                           <>
                             <MenuItem>
                               <Link
