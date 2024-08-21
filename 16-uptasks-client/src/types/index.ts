e865b283f6a9dbf9ff328d8e4c662d16c399a5e3
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-20 20:37:02 -0400

    Muestra autor de la última actulización de la tarea

--- 16-uptasks-client/src/types/index.ts
+++ 16-uptasks-client/src/types/index.ts
@@ -42,6 +42,7 @@ export const taskSchema = z.object({
   description: z.string(),
   project: z.string(),
   status: taskStatusSchema,
+  completedBy: userSchema.or(z.null()),
   createdAt: z.string(),
   updatedAt: z.string(),
 });
