2c0c15b6cd8a871fdd2207da6618541aeb1faed2
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-22 14:12:39 -0400

    Pagina 404

--- 16-uptasks-client/src/router.tsx
+++ 16-uptasks-client/src/router.tsx
@@ -15,6 +15,7 @@ import ProfileView from './views/profile/ProfileView';
 import ProjectDetailsView from './views/projects/ProjectDetailsView';
 import ProjectTeamView from './views/projects/ProjectTeamView';
 import RegisterView from './views/auth/RegisterView';
+import NotFound from './views/404/NotFound';
 
 export default function Router() {
   return (
@@ -39,6 +40,9 @@ export default function Router() {
           <Route path="/auth/request-code" element={<RequestNewCodeView />} />
           <Route path="/auth/forgot-password" element={<ForgotPasswordView />} />
           <Route path="/auth/new-password" element={<NewPasswordView />} />
+        </Route>
+        <Route element={<AuthLayout />}>
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
