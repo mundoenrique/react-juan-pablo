407392ad0052649fc71dd5860dbdc27665cd54b1
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-22 09:44:32 -0400

    Actulización de perfil

--- 15-uptasks-server/src/controllers/AuthController.ts
+++ 15-uptasks-server/src/controllers/AuthController.ts
@@ -217,4 +217,62 @@ export class AuthController {
   static user = async (req: Request, res: Response) => {
     return res.json(req.user);
   };
+
+  static updateProfile = async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists && userExists.id.toString() !== req.user.id.toString()) {
+      const error = new Error('Ese email ya esta registrado');
+
+      return res.status(409).json({ error: error.message });
+    }
+
+    req.user.name = name;
+    req.user.email = email;
+
+    try {
+      await req.user.save();
+
+      res.send('Perfil actualizado correctamente');
+    } catch (error) {
+      res.status(500).send('Hubo un error');
+    }
+  };
+
+  static updateCurrentUserPassword = async (req: Request, res: Response) => {
+    const { current_password, password } = req.body;
+
+    const user = await User.findById(req.user.id);
+    const isPasswordCorrect = await checkPassword(current_password, user.password);
+
+    if (!isPasswordCorrect) {
+      const error = new Error('El Password actual es incorrecto');
+
+      return res.status(401).json({ error: error.message });
+    }
+
+    try {
+      user.password = await hashPassword(password);
+      await user.save();
+      res.send('El Password se modificó correctamente');
+    } catch (error) {
+      res.status(500).send('Hubo un error');
+    }
+  };
+
+  static checkPassword = async (req: Request, res: Response) => {
+    const { password } = req.body;
+
+    const user = await User.findById(req.user.id);
+    const isPasswordCorrect = await checkPassword(password, user.password);
+
+    if (!isPasswordCorrect) {
+      const error = new Error('El Password es incorrecto');
+      return res.status(401).json({ error: error.message });
+    }
+
+    res.send('Password Correcto');
+  };
 }
--- 15-uptasks-server/src/routes/authRoutes.ts
+++ 15-uptasks-server/src/routes/authRoutes.ts
@@ -75,4 +75,36 @@ router.post(
 
 router.get('/user', authenticate, AuthController.user);
 
+router.put(
+  '/profile',
+  authenticate,
+  body('name').notEmpty().withMessage('El nombre no puede ir vacio'),
+  body('email').isEmail().withMessage('E-mail no válido'),
+  handleInputErrors,
+  AuthController.updateProfile
+);
+
+router.post(
+  '/update-password',
+  authenticate,
+  body('current_password').notEmpty().withMessage('El password actual no puede ir vacio'),
+  body('password').isLength({ min: 8 }).withMessage('El password es muy corto, minimo 8 caracteres'),
+  body('password_confirmation').custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Los Password no son iguales');
+    }
+    return true;
+  }),
+  handleInputErrors,
+  AuthController.updateCurrentUserPassword
+);
+
+router.post(
+  '/check-password',
+  authenticate,
+  body('password').notEmpty().withMessage('El password no puede ir vacio'),
+  handleInputErrors,
+  AuthController.checkPassword
+);
+
 export default router;
