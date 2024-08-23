04a79ef67a4126369c37835434f03d57b6a2b8e9
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-23 09:34:39 -0400

    Prepara el quiosco

---
+++ 17-quiosco-next/app/order/page.tsx
@@ -0,0 +1,3 @@
+export default function Orderpage() {
+  return <div>Orderpage</div>;
+}
---
+++ 17-quiosco-next/components/order/OrderSidebar.tsx
@@ -0,0 +1,7 @@
+export default function OrderSidebar() {
+  return (
+    <aside className="md:w-72 md:h-screen bg-white">
+      <nav className="mt-10">OrderSidebar</nav>
+    </aside>
+  );
+}
