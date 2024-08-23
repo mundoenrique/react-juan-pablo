b3c4c6b910ac5e3363411b1ae38d0d947ddb532e
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-23 15:10:36 -0400

    Store sustand

---
+++ 17-quiosco-next/src/types/index.ts
@@ -0,0 +1,12 @@
+import { Product } from '@prisma/client';
+
+export type OrderItem = Pick<Product, 'id' | 'name' | 'price'> & {
+  quantity: number;
+  subtotal: number;
+};
+
+// export type OrderWithProducts = Order & {
+//   orderProducts: (OrderProducts & {
+//     product: Product;
+//   })[];
+// };
