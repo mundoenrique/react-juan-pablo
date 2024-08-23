5d2ed62314682dd2af624b35379c01aa2d35fbdf
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-23 11:27:44 -0400

    Instancia prisma

--- 17-quiosco-next/components/order/OrderSidebar.tsx
+++ 17-quiosco-next/components/order/OrderSidebar.tsx
@@ -1,4 +1,12 @@
-export default function OrderSidebar() {
+import { prisma } from '@/src/lib/prisma';
+
+async function getCategories() {
+  return await prisma.category.findMany();
+}
+
+export default async function OrderSidebar() {
+  const categories = await getCategories();
+
   return (
     <aside className="md:w-72 md:h-screen bg-white">
       <nav className="mt-10">OrderSidebar</nav>
