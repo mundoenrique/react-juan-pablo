96cf078e868a28ac1b65b96f6b1bb5e9d46c5eed
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-23 14:49:13 -0400

    Renderiza productos por categoria

---
+++ 17-quiosco-next/app/order/[category]/page.tsx
@@ -0,0 +1,30 @@
+import ProductCard from '@/components/products/ProductCard';
+import { prisma } from '@/src/lib/prisma';
+
+async function getProducts(category: string) {
+  console.log(category);
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category,
+      },
+    },
+  });
+
+  return products;
+}
+
+export default async function Orderpage({ params }: { params: { category: string } }) {
+  const products = await getProducts(params.category);
+
+  return (
+    <>
+      <h1 className="text-2xl my-10">Elige y personaliza tu pedido a continuación</h1>
+      <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-4 gap-4 items-start">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
+}
--- 17-quiosco-next/app/order/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Orderpage() {
-  return <div>Orderpage</div>;
-}
