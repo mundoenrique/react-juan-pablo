589747c3e2cbdb86f054ade3b2a7972fff40a6d7
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-23 16:46:12 -0400

    Crea la orden

--- 17-quiosco-next/components/products/ProductCard.tsx
+++ 17-quiosco-next/components/products/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { formatCurrency } from '@/src/utils';
 import { Product } from '@prisma/client';
 import Image from 'next/image';
+import AddProductButton from './AddProductButton';
 
 type ProductCardProps = {
   product: Product;
@@ -13,12 +14,7 @@ export default function ProductCard({ product }: ProductCardProps) {
       <div className="p-5">
         <h3 className="text-2xl font-bold">{product.name}</h3>
         <p className="mt-5 font-black text-4xl text-amber-500">{formatCurrency(product.price)}</p>
-        <button
-          type="button"
-          className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
-        >
-          Agregar
-        </button>
+        <AddProductButton product={product} />
       </div>
     </div>
   );
