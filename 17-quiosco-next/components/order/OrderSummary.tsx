da3fb52aaac90b72240c058648a148febb0caad9
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-23 17:34:02 -0400

    Formulario para crear la orden

--- 17-quiosco-next/components/order/OrderSummary.tsx
+++ 17-quiosco-next/components/order/OrderSummary.tsx
@@ -8,6 +8,16 @@ export default function OrderSummary() {
   const order = useStore((state) => state.order);
   const total = useMemo(() => order.reduce((total, item) => total + item.quantity * item.price, 0), [order]);
 
+  const handleCreateOrder = async (formData: FormData) => {
+    const data = {
+      name: formData.get('name'),
+      total,
+      order,
+    };
+
+    console.log(data);
+  };
+
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
@@ -23,6 +33,21 @@ export default function OrderSummary() {
             Total a pagar: {''}
             <span className="font-bold">{formatCurrency(total)}</span>
           </p>
+
+          <form className="w-full mt-10 space-y-5" action={handleCreateOrder}>
+            <input
+              type="text"
+              placeholder="Tu Nombre"
+              className="bg-white border border-gray-100 p-2 w-full"
+              name="name"
+            />
+
+            <input
+              type="submit"
+              className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
+              value="Confirmar Pedido"
+            />
+          </form>
         </div>
       )}
     </aside>
