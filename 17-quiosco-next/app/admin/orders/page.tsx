93eccb28dd4259349d18de0d6087b0bcf2428083
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-23 22:33:22 -0400

    REnderiza las ordenes y las mar completadas

--- 17-quiosco-next/app/admin/orders/page.tsx
+++ 17-quiosco-next/app/admin/orders/page.tsx
@@ -1,9 +1,40 @@
+import OrderCard from '@/components/order/OrderCard';
 import Heading from '@/components/ui/Heading';
+import { products } from '@/prisma/data/products';
+import { prisma } from '@/src/lib/prisma';
 
-export default function OrdersPage() {
+async function getPendingOrders() {
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false,
+    },
+    include: {
+      orderProducts: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  return orders;
+}
+
+export default async function OrdersPage() {
+  const orders = await getPendingOrders();
+
   return (
     <>
       <Heading>Administrar Ordenes</Heading>
+      {orders.length ? (
+        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+          {orders.map((order) => (
+            <OrderCard key={order.id} order={order} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-center">No hay ordenes Pendientes</p>
+      )}
     </>
   );
 }
--- 17-quiosco-next/src/schema/index.ts
+++ 17-quiosco-next/src/schema/index.ts
@@ -13,3 +13,10 @@ export const OrderSchema = z.object({
     })
   ),
 });
+
+export const OrderIdSchema = z.object({
+  orderId: z
+    .string()
+    .transform((value) => parseInt(value))
+    .refine((value) => value > 0, { message: 'Hay errores' }),
+});
