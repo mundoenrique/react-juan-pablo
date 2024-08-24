f1495604d67984a39ab3f325e33d1711de38bba6
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-24 18:05:00 -0400

    Obtiene productos paginados

--- 17-quiosco-next/app/admin/products/page.tsx
+++ 17-quiosco-next/app/admin/products/page.tsx
@@ -1,9 +1,34 @@
+import { redirect } from 'next/navigation';
+// import ProductsPagination from '@/components/products/ProductsPagination';
+import ProductTable from '@/components/products/ProductsTable';
 import Heading from '@/components/ui/Heading';
+import { prisma } from '@/src/lib/prisma';
+import Link from 'next/link';
+// import ProductSearchForm from '@/components/products/ProductSearchForm';
 
-export default async function ProductsPage() {
+async function getProducts(page: number, pageSize: number) {
+  const skip = (page - 1) * pageSize;
+  const products = await prisma.product.findMany({
+    take: pageSize,
+    skip,
+    include: {
+      category: true,
+    },
+  });
+  return products;
+}
+
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>;
+
+export default async function ProductsPage({ searchParams }: { searchParams: { page: string } }) {
+  const page = +searchParams.page || 1;
+  const pageSize = 10;
+  const products = await getProducts(page, pageSize);
+
   return (
     <>
       <Heading>Administrar Productos</Heading>
+      <ProductTable products={products} />
     </>
   );
 }
