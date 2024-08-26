fcfc75f843ed71a6cfe7ffa10b0081f3871d566b
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-26 11:41:11 -0400

    Agre painación y mapeo de pagina actual

--- 17-quiosco-next/app/admin/products/page.tsx
+++ 17-quiosco-next/app/admin/products/page.tsx
@@ -1,10 +1,14 @@
 import { redirect } from 'next/navigation';
-// import ProductsPagination from '@/components/products/ProductsPagination';
+import ProductsPagination from '@/components/products/ProductsPagination';
 import ProductTable from '@/components/products/ProductsTable';
 import Heading from '@/components/ui/Heading';
 import { prisma } from '@/src/lib/prisma';
 import Link from 'next/link';
 // import ProductSearchForm from '@/components/products/ProductSearchForm';
+
+async function productCount() {
+  return await prisma.product.count();
+}
 
 async function getProducts(page: number, pageSize: number) {
   const skip = (page - 1) * pageSize;
@@ -23,12 +27,18 @@ export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>;
 export default async function ProductsPage({ searchParams }: { searchParams: { page: string } }) {
   const page = +searchParams.page || 1;
   const pageSize = 10;
-  const products = await getProducts(page, pageSize);
+  if (page < 0) redirect('/admin/products');
+  const productsData = getProducts(page, pageSize);
+  const totalProductsData = productCount();
+  const [products, totalProducts] = await Promise.all([productsData, totalProductsData]);
+  const totalPages = Math.ceil(totalProducts / pageSize);
+  if (page > totalPages) redirect('/admin/products');
 
   return (
     <>
       <Heading>Administrar Productos</Heading>
       <ProductTable products={products} />
+      <ProductsPagination page={page} totalPages={totalPages} />
     </>
   );
 }
