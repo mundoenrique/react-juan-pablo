aa6c4919bd04b35e285dd0246412deb720e46984
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-26 12:26:27 -0400

    Formulario para buscar productos

--- 17-quiosco-next/app/admin/products/page.tsx
+++ 17-quiosco-next/app/admin/products/page.tsx
@@ -4,7 +4,7 @@ import ProductTable from '@/components/products/ProductsTable';
 import Heading from '@/components/ui/Heading';
 import { prisma } from '@/src/lib/prisma';
 import Link from 'next/link';
-// import ProductSearchForm from '@/components/products/ProductSearchForm';
+import ProductSearchForm from '@/components/products/ProductSearchForm';
 
 async function productCount() {
   return await prisma.product.count();
@@ -37,6 +37,16 @@ export default async function ProductsPage({ searchParams }: { searchParams: { p
   return (
     <>
       <Heading>Administrar Productos</Heading>
+      <div className="flex flex-col lg:flex-row lg:justify-between gap-5">
+        <Link
+          href={'/admin/products/new'}
+          className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
+        >
+          Crear Producto
+        </Link>
+
+        <ProductSearchForm />
+      </div>
       <ProductTable products={products} />
       <ProductsPagination page={page} totalPages={totalPages} />
     </>
