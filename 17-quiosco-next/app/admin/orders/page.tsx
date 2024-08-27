a2e6bba19c2e121bf65e7970be83883a0b2154a1
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-27 08:45:42 -0400

    Revalida lista de ordenes

--- 17-quiosco-next/app/admin/orders/page.tsx
+++ 17-quiosco-next/app/admin/orders/page.tsx
@@ -1,40 +1,36 @@
+'use client';
+import useSWR from 'swr';
 import OrderCard from '@/components/order/OrderCard';
 import Heading from '@/components/ui/Heading';
-import { products } from '@/prisma/data/products';
-import { prisma } from '@/src/lib/prisma';
+import { OrderWithProducts } from '@/src/types';
 
-async function getPendingOrders() {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false,
-    },
-    include: {
-      orderProducts: {
-        include: {
-          product: true,
-        },
-      },
-    },
+export default function OrdersPage() {
+  const url = '/admin/orders/api';
+  const fetcher = () =>
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => data);
+  const { data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+    refreshInterval: 1000,
+    revalidateOnFocus: false,
   });
 
-  return orders;
-}
+  if (isLoading) return <p>Cargando...</p>;
 
-export default async function OrdersPage() {
-  const orders = await getPendingOrders();
+  if (data)
+    return (
+      <>
+        <Heading>Administrar Ordenes</Heading>
 
-  return (
-    <>
-      <Heading>Administrar Ordenes</Heading>
-      {orders.length ? (
-        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-          {orders.map((order) => (
-            <OrderCard key={order.id} order={order} />
-          ))}
-        </div>
-      ) : (
-        <p className="text-center">No hay ordenes Pendientes</p>
-      )}
-    </>
-  );
+        {data.length ? (
+          <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+            {data.map((order) => (
+              <OrderCard key={order.id} order={order} />
+            ))}
+          </div>
+        ) : (
+          <p className="text-center">No hay ordenes Pendientes</p>
+        )}
+      </>
+    );
 }
