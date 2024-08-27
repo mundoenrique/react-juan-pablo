0b68cc5f506872cd0b31b08ad4718b5ff4de9f58
mundoenrique <enrique.penaloza.working@gmail.com>
2024-08-27 07:39:16 -0400

    Crea un producto

--- 17-quiosco-next/components/products/ProductCard.tsx
+++ 17-quiosco-next/components/products/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { formatCurrency } from '@/src/utils';
+import { formatCurrency, getImagePath } from '@/src/utils';
 import { Product } from '@prisma/client';
 import Image from 'next/image';
 import AddProductButton from './AddProductButton';
@@ -8,9 +8,11 @@ type ProductCardProps = {
 };
 
 export default function ProductCard({ product }: ProductCardProps) {
+  const imagePath = getImagePath(product.image);
+
   return (
     <div className="border bg-white">
-      <Image width={400} height={500} src={`/products/${product.image}.jpg`} alt={`Imagen platillo ${product.name}`} />
+      <Image width={400} height={500} src={imagePath} alt={`Imagen platillo ${product.name}`} />
       <div className="p-5">
         <h3 className="text-2xl font-bold">{product.name}</h3>
         <p className="mt-5 font-black text-4xl text-amber-500">{formatCurrency(product.price)}</p>
